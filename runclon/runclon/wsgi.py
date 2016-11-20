26d677676aeae62b8123f56d5f955d29ab6d6807
Rob <r.nilshilliard@gmail.com>
2016-11-20 17:48:51 +0100

    Path change

--- runclon/runclon/wsgi.py
+++ runclon/runclon/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "runclon/runclon.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "runclon.runclon.settings")
 
 application = get_wsgi_application()
